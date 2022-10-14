1ffe1be81d1cddd83b5dec86e3d0cca56cccb5bf
Addison Mink <addison.mink@example.com>
2022-10-14 14:43:51 -0500

    add goblin sprite

--- src/main/scala/amink/rpg/Main.scala
+++ src/main/scala/amink/rpg/Main.scala
@@ -17,11 +17,15 @@ import amink.canvasui.*
   val init = {
     import battle.model.*
     import util.*
+    val sprite = Sprite("goblin", null, 32, 32, 16, 16, 32, 32)
+    val src = "images/goblin.png"
     val weapon = Species.goblinBow
     val player1 = Creature.make(0, "1", Species.Fighter(weapon), Row.Front)
     val player2 = Creature.make(1, "2", Species.Fighter(weapon), Row.Back)
     val cMap = Map(0 -> player1, 1 -> player2)
-    State.LoadingSprites(Seed.Cycle(Nil), cMap, Map(), Map())
+    val sprites = Map("goblin" -> sprite)
+    val sources = Map("goblin" -> src)
+    State.LoadingSprites(Seed.Cycle(Nil), cMap, sprites, sources)
   }
 
   val engine = {
