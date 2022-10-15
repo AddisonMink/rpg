34536783f998f40c3253bd23baaa1a890a56b594
Addison Mink <addison.mink@example.com>
2022-10-15 13:11:50 -0500

    implement monster view

---
+++ src/main/scala/amink/rpg/battle/view/MonsterView.scala
@@ -0,0 +1,45 @@
+package amink.rpg.battle.view
+
+import amink.canvasui.*
+
+import amink.rpg.battle.model.*
+
+final case class MonsterView(
+    monster: Creature,
+    sprite: Sprite
+):
+  import MonsterView.*
+  import ComponentUtils.AlignmentH
+  import ComponentUtils.AlignmentV
+
+  def component: Component =
+    val alignV = monster.row match
+      case Row.Front => AlignmentV.Bottom
+      case Row.Back  => AlignmentV.Top
+
+    val realSprite = monster.row match
+      case Row.Front =>
+        sprite.copy(
+          canvasFrameWidth = sprite.canvasFrameWidth * 2,
+          canvasFrameHeight = sprite.canvasFrameHeight * 2
+        )
+      case Row.Back => sprite
+
+    val frame = ComponentUtils.spriteFrame(realSprite, (0, 0))
+
+    ComponentUtils.box(
+      frame,
+      width,
+      height,
+      alignH = AlignmentH.Center,
+      alignV = AlignmentV.Center
+    )
+
+object MonsterView:
+  val width: Int = 60
+  val height: Int = 100
+
+  def make(state: State, id: Id): MonsterView =
+    val creature = state.creatureMap(id)
+    val sprite = state.sprites(creature.species.name)
+    MonsterView(creature, sprite)
--- src/main/scala/amink/rpg/battle/view/View.scala
+++ src/main/scala/amink/rpg/battle/view/View.scala
@@ -6,12 +6,22 @@ import amink.rpg.battle.model.*
 import amink.canvasui.ComponentUtils.AlignmentH
 
 final case class View(
+    monsters: List[MonsterView],
     players: List[PlayerView]
 ):
   import View.*
 
   def component: Component =
-    playerRow
+    Style.column(List(monsterRow, playerRow))
+
+  private def monsterRow: Component =
+    val row = Style.row(monsters.map(_.component))
+    ComponentUtils.box(
+      row,
+      minWidth = playerRowWidth,
+      minHeight = MonsterView.height,
+      alignH = AlignmentH.Center
+    )
 
   private def playerRow: Component =
     val row = Style.row(players.map(_.component))
@@ -24,7 +34,14 @@ final case class View(
 
 object View:
   val playerRowWidth: Int = 4 * PlayerView.height + 3 * Style.rowMargin
+  val width: Int = playerRowWidth
+  val height: Int = PlayerView.height + MonsterView.height + Style.columnMargin
 
-  def make(state: State): View = View(
-    players = state.creatureMap.players.map(PlayerView(_))
-  )
+  def make(state: State): View =
+    val monsters = state.creatureMap.monsters
+      .map(m => MonsterView.make(state, m.id))
+
+    val players = state.creatureMap.players
+      .map(PlayerView(_))
+
+    View(monsters, players)
